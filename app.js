5ac4b872b93f59cc206071110750e74e4944b7ff
Wolf-Tungsten <canis.tungsten@foxmail.com>
2019-01-24 09:34:09 +0800

    调整提示

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ http.createServer((request, response) => {
                 responseBody.msg = '统一身份认证成功'
                 responseBody.success = true
             } else if (code === 0) {
-                responseBody.msg = '用户名或密码错误'
+                responseBody.msg = '一卡通或密码错误'
                 responseBody.success = false
             } else if (code === -1) {
-                responseBody.msg = '安全保护，请前往信息门户界面登陆'
+                responseBody.msg = '出于安全性考虑，本次登录被临时限制。请使用浏览器访问并重新登录校园信息门户（my.seu.edu.cn），消除验证码后再进行尝试。'
                 responseBody.success = false
             }
             return responseBody
